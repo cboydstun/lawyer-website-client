fe17b7b0db3ef68162b0b914fa3be1f09ede1df8
ddsykes <ddsykes@hotmail.com>
2024-09-04 19:19:25 -0500

    Adding Changes to the Nav.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 //Importing the libraries for React
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import './Navbar.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button, Navbar as Nav } from "react-daisyui";
 
 //Creating the functions for the Navbar
 const Navbar = () => {
- const [click, setClick]   = useState(false);
- const handleClick = () => setClick(!click);
- const closeMobileMenu = () => setClick(false);
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-   <>
-   <nav className="navbar">
+    <>
+      {/* <nav className="navbar">
  <div className="navbar-container">
 <Link to="/" className="navbar-logo">
-Jacqueline R. Kriebel
+Jacqueline R. Kriebel  <i className="fab fa-typo3"/>
 </Link>
 <div className="menu-icon" onClick={handleClick}>
    <i className={click ? 'fas fa-times': 'fas fa-bars'} />
@@ -51,10 +52,57 @@ Jacqueline R. Kriebel
     </Link>
    </li>
  </div>
-   </nav>
-   </>
-  )
-}
+   </nav> */}
+      <Nav className="bg-primary min-w-full w-screen">
+        <div className="flex-none">
+          <Button shape="square" color="ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </Button>
+        </div>
+        <Nav.Start>
+          <Button tag="a" color="secondary" className="normal-case text-xl">
+            daisyUI
+          </Button>
+        </Nav.Start>
+        <Nav.End>
+          <div className="flex gap-2">
+            <Link to="/home" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+
+            <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+              About
+            </Link>
+            <Link to='/attorneyprofile' className='nav-links' onClick={closeMobileMenu}>
+    Attorney Profile
+    </Link>
+    <Link to='/practieareas' className='nav-links' onClick={closeMobileMenu}>
+    Practice Areas
+    </Link>
+    <Link to='/clienttestimonials' className='nav-links' onClick={closeMobileMenu}>
+  Client Testimonials
+    </Link>
+    <Link to='/more' className='nav-links' onClick={closeMobileMenu}>
+   More
+    </Link>
+          </div>
+        </Nav.End>
+      </Nav>
+    </>
+  );
+};
 
 //Exporting the default Navbar
-export default Navbar
+export default Navbar;
