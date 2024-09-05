be8e7366fe027ede73a7edfc76125fe4c57a1d66
ddsykes <ddsykes@hotmail.com>
2024-09-05 17:36:56 -0500

    Adding changes to the footer and Nav.

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,24 @@ import "./components/Navbar.jsx"
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar.jsx";
 import HeroSection from "./components/HeroSection.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
   <>
-
  <Router>
   <Navbar/>
+  <div style={{minHeight:"65vh"}}>
   <HeroSection/>
+  
+  
   <Routes>
     <Route path='/' exact/>
   </Routes>
+  </div>
+  <Footer/>
   </Router>
-
+  
   </>
 )
 }
