e05be492d04d7edd1948713d80873a480bb79f97
CodeBelow89 <ririck101@gmail.com>
2024-09-05 20:28:42 -0400

    Before merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import "./App.css";
 import "../../server/index.js";
-import "./components/Navbar.jsx"
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import "./components/Navbar.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-  <>
-  <h2>Website</h2>
- <Router>
-  <Navbar/>
-  <Routes>
-    
-    <Route path='/' exact/>
-  </Routes>
-  </Router>
-  </>
-)
+    <>
+      <h2>Website</h2>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
