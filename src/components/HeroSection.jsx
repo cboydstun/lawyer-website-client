ae071176147e7a069b42def3c76bb8143206079f
ddsykes <ddsykes@hotmail.com>
2024-09-11 17:31:02 -0500

    Adding Changes to website.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div className='hero-container'>
+      <div>
+        <img src="" alt="Law Firm Image" />
+      </div>
       <h1>Hero Section of Website</h1>
     </div>
   )
