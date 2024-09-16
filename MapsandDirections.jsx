19c402ab5f4ebe44e5bc1d95f87598af3902cfca
ddsykes <ddsykes@hotmail.com>
2024-09-16 18:22:41 -0500

    Adding to the Website.

---
+++ MapsandDirections.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const MapsandDirections = () => {
+  return (
+    <div>
+      MapsandDirections
+      <div>
+        <p>
+          Kriebel Law Firm, PLLC San Antonio Office Blanco Plaza 6609 Blanco
+          Rd., Ste. 100 San Antonio, Texas 78216 Phone: 210-677-5586 Fax:
+          210-467-5788 To get directions to this location, click "Directions" or
+          "View larger map" on the map below (Google Maps will open in a new
+          window).
+        </p>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default MapsandDirections;
