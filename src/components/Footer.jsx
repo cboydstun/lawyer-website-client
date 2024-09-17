1b261bf0fbd458770506043aaf585be830229828
awhitbourn <aprilwhitbourn@gmail.com>
2024-09-16 20:39:44 -0400

    finishing up footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React from "react"; //Import React
 import "./Footer.css"; //Import CSS file
-import JCicon from "../assets/photos2.jpg"
+import JCicon from "../assets/photos2.jpg" //Import photo
 
+//Creating review section
 const reviews = [
   "Powerhouse!!! Amazing, worth every penny!! Justice was served!!! You can't go wrong hiring her!!!! I'm so grateful for her!!!! - Heather M",
   "Jackie Kribel is an outstanding attorney. If you want an attorney that will remove the fear and doubt you may have, she does just that. Being in a legal battle is scary. Jackie Kribel put my mind at ease. She is confident & experienced and I promise exactly what you need when you need legal representation. Thank you Kribel Law Firm for you outstanding attention to detail and perfect representation in my case. - Rich R.",
@@ -11,6 +12,7 @@ const reviews = [
   "She’s feisty and a go getter. So glad I went with her! She answered my call on a weekend which I wasn’t expecting! Allowed me to communicate my concern bc I had no idea what I was looking at, at the time. She met with me the following Monday! She plays no games and she is great at what she does! Don’t think about it, just do it!- Emily",
 ];
 
+//Creating footer section
 const Footer = () => {
   return (
     <div>
@@ -19,8 +21,11 @@ const Footer = () => {
         <img 
           src={JCicon} 
           alt="icon image" 
-          className="w-24 h-24 mb-4"
+          className=" absolute left-4 w-24 h-26 mb-4"
         />
+       <a href="lawyer-website-client\src\components\Attorneyprofile.jsx" className="text-yellow-300 underline mb-4 absolute left-4 py-40">
+        Attorney Profile
+      </a>
       <div className="text-center mb-8">
         <h2 className="text-xl font-bold mb-4">Client Reviews</h2>
       <div className="space-y-4">
@@ -34,7 +39,7 @@ const Footer = () => {
   </div>
   <div className="text-center mt-4 text-lg">
   <p>Take Control of Your Legal Situation</p>
-  <p className="font-semibold mt-2">Book your Consultation: <span className="text-yellow-500">210-677-5586</span></p>
+  <p className="font-semibold mt-2">Book your Consultation: <span className="text-yellow-300">210-677-5586</span></p>
   </div>
 </footer>
 </div>
